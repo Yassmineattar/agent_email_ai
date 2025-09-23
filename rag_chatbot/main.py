221194cdf588c17ec390d39ea68045e7e965e06a
Yassmineattar <yassmineattar13@gmail.com>
2025-09-23 20:02:32 +0100

    refactor: improve query parameter handling

--- rag_chatbot/main.py
+++ rag_chatbot/main.py
@@ -1,13 +1,15 @@
 import streamlit as st
-from utils.auth_system import AuthSystem
 from client_interface import client_app
 from admin_interface import admin_app
 
 def main():
-    """Point d'entrée principal - Redirection vers l'app appropriée"""
-    query_params = st.query_params
+    """Point d'entrée principal"""
+    query_params = st.query_params if hasattr(st, 'query_params') else st.experimental_get_query_params()
     
-    if "admin" in query_params:
+    # Vérifier le paramètre admin
+    show_admin = "admin" in query_params
+    
+    if show_admin:
         admin_app()
     else:
         client_app()
